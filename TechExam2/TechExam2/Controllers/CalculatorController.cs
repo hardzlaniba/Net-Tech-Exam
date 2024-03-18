a4d4e8483a09c8142eb09805ac3c96ae8025a27a
hardzlaniba <44597557+hardzlaniba@users.noreply.github.com>
2024-03-18 17:50:48 +0800

    Add files via upload

---
+++ TechExam2/TechExam2/Controllers/CalculatorController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using TechExam2.Interface;
+using TechExam2.Model;
+using TechExam2.Service;
+
+namespace TechExam2.Controllers
+{
+    [ApiController]
+    [Route("api/")]
+    public class CalculatorController : ControllerBase
+    {
+        private readonly ICalculatorService _iCalculatorService;
+        public CalculatorController(ICalculatorService iCalculatorService)
+        {
+            _iCalculatorService = iCalculatorService;
+        }
+        [HttpPost("RoundSumOfTwoNumber")]
+        //public async IActionResult RoundSumOfTwoNumber(CalculateRequest request)
+        public async Task<ActionResult> RoundSumOfTwoNumber(CalculateRequest request)
+        {
+            CalculateResponse calculateResponse = new CalculateResponse();
+            //apply a task.run method here since we already had omitted it in the service and dump the results to the model.
+            calculateResponse.Answer = await Task.Run(() => _iCalculatorService.RoundingSumOf2Int(request.FirstNumber, request.SecondNumber)).ConfigureAwait(false);
+
+            return Ok(calculateResponse);
+        }
+    }
+}
---
+++ TechExam2/TechExam2/Service/CalculatorService.cs
@@ -0,0 +1,25 @@
+﻿using TechExam2.Interface;
+
+namespace TechExam2.Service
+{
+    public class CalculatorService : ICalculatorService
+    {
+        //need to remove task because this method return an int and completely synchronous. The async await method is now define in the controller.
+        public int RoundingSumOf2Int(int firstNumber, int secondNumber)
+        {
+            int roundedValue = 0;
+            int sum = 0;
+            //declare a var used for the multiple of 5
+            int nextMultiplier = 0;
+
+            //no need to initialize another parameter because var sum is already defined above
+            sum = firstNumber + secondNumber;
+            //get the next multiple of 5 by just dividing the numbers to 5 to get the whole number quotient, then multiple it by 5 and adding by 5.
+            nextMultiplier = ((sum / 5) * 5) + 5;
+            //applying the condition if the difference between the nextmultiplier and the sum is less than 3 then get the value of the nextmultiplier, else simply get the sum of the first and 2nd number.
+            roundedValue = (nextMultiplier - sum) < 3 ? nextMultiplier : sum;
+
+            return roundedValue;
+        }
+    }
+}
